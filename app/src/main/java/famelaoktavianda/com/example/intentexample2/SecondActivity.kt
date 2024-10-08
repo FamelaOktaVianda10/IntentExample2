21685abe3d77be7539a77c143cff98c6aab0fdd1
LOQ <jinemoch@gmail.com>
2024-10-08 12:32:20 +0700

    First Commit

---
+++ app/src/main/java/famelaoktavianda/com/example/intentexample2/SecondActivity.kt
@@ -0,0 +1,18 @@
+package famelaoktavianda.com.example.intentexample2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import famelaoktavianda.com.example.intentexample2.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val receiveData = intent.getStringExtra("main_activity_data")
+        binding.textView.text = receiveData
+    }
+}
